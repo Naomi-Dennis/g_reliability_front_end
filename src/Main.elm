aa11ef754456b503566de7a42c5ece02b12826c9
Naomi Dennis <naomi@8thlight.com>
2020-03-18 17:36:32 -0400

    Retrieve data from local host endpoint

--- src/Main.elm
+++ src/Main.elm
@@ -3,50 +3,22 @@ port module Main exposing (elmToJS)
 import ArrivalTime exposing (..)
 import Browser
 import Html exposing (Html)
+import Http exposing (..)
 import Json.Decode as Decode exposing (decodeString)
 import Platform
 import VegaLite exposing (..)
 
 
-fakeArrivalData : String
-fakeArrivalData =
-    """{"day": [
-            "7 Days Before",
-            "6 Days Before",
-            "5 Days Before",
-            "4 Days Before",
-            "3 Days Before",
-            "2 Days Before",
-            "Yesterday",
-            "Today"
-          ],
-        "meanArrivalDifferences": [
-                2.34,
-                -0.10,
-                5.30,
-                7.20,
-                -5.32,
-                -4.20,
-                0.00,
-                0.32
-          ]
-        }"""
-
-
-decodedData =
-    decodeString arrivalTimeDecoder fakeArrivalData
-
-
-fakeArrivalTimes =
-    validateArrivalData decodedData
+type Response
+    = ReceivedData (Result Http.Error ArrivalTimes)
 
 
 xAxisName =
-    "Day"
+    "day"
 
 
 yAxisName =
-    "Mean Difference in Estimated vs Actual Arrival Time (Minutes)"
+    "meanArrivalDifferences"
 
 
 axisFontSize =
@@ -69,16 +41,14 @@ arrivalBarGraph : Spec
 arrivalBarGraph =
     let
         des =
-            description "A simple bar chart with embedded data."
+            description "A visualization of arrival time averages for the MTA G Train Greenpoint Ave. stop"
 
         data =
-            dataFromColumns []
-                << dataColumn xAxisName (strs fakeArrivalTimes.day)
-                << dataColumn yAxisName (nums fakeArrivalTimes.meanArrivalDifferences)
+            dataFromUrl "http://localhost:4000/" []
 
         enc =
             encoding
-                << position X [ pName xAxisName, pOrdinal, pSort [ soCustom (strs fakeArrivalTimes.day) ], pAxis [ axTitleFontSize axisFontSize ] ]
+                << position X [ pName xAxisName, pOrdinal, pSort [], pAxis [ axTitleFontSize axisFontSize ] ]
                 << position Y [ pName yAxisName, pQuant, pAxis [ axTitleFontSize axisFontSize ] ]
 
         br =
@@ -94,10 +64,10 @@ arrivalBarGraph =
             configure
                 << configuration (coAxis [ axcoLabelFontSize axisLabelFontSize ])
     in
-    toVegaLite [ graphTitle, width 500, height 450, cfg [], size, des, data [], enc [], br ]
+    toVegaLite [ graphTitle, width 500, height 450, cfg [], size, des, data, enc [], br ]
 
 
-main : Program () Spec msg
+main : Program () Spec Response
 main =
     Platform.worker
         { init = always ( arrivalBarGraph, elmToJS arrivalBarGraph )
